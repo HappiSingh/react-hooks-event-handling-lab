640ebcf7050d4b3884349f31dbace6dbdcf10937
Harpreet Singh <Harpreets1201@gmail.com>
2023-05-29 03:36:46 -0400

    Completed lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <h1>Keypad</h1>
+            <input type="password" onChange={() => console.log("Entering password...")}>
+
+            </input>
+        </div>
     )
 }
 
